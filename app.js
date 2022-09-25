2328066e324dc5fc390107deac4048acfd3344be
evvlboro <be@bchc.app>
2022-09-25 23:00:56 +0300

    fix: some fixes

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const DataNotFoundError = require('./errors/DataNotFoundError');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 
 const app = express();
+
+const originDev = [
+  'http://localhost',
+  'http://localhost:3000', // dev
+];
+
+app.use(cors(originDev));
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
